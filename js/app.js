6b7f99df2f3dc5458fa19e724f5f2b3dd2cace46
asadbekdev07 <futureprotrader1007@gmail.com>
2024-05-04 12:27:39 +0500

    Add strong attack  and heal functions.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const ATTACK_VALUE = 10
-const MONSTER_ATTACK_VALUE = 10
+const STRONG_ATTACK_VALUE = 17
+const MONSTER_ATTACK_VALUE = 14
+const HEAL_VALUE = 20
 
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -7,10 +9,7 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE)
-  currentMonsterHealth -= damage
-
+function endRound() {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
   currentPlayerHealth -= playerDamage
 
@@ -23,4 +22,41 @@ function attackHandler() {
   }
 }
 
-attackBtn.addEventListener("click", attackHandler)
+function attackMonster(mode) {
+  let maxDamage;
+  if(mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE
+  }
+
+  const damage = dealMonsterDamage(maxDamage)
+  currentMonsterHealth -= damage
+
+  endRound()
+}
+
+function attackHandler() {
+  attackMonster("ATTACK")
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK")
+}
+
+function healPlayerHandler() {
+  let healValue
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("You can't heal to more than your max initial health")
+    healValue = chosenMaxLife - currentPlayerHealth
+  } else {
+    healValue = HEAL_VALUE
+  }
+  increasePlayerHealth(HEAL_VALUE)
+  currentPlayerHealth += HEAL_VALUE
+  endRound()
+}
+
+attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
